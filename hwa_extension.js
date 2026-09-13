cabf74502977a7e04b88ba73385555d66234f0d4
Dmitry <dmitryprg@gmail.com>
2026-09-13 09:37:53 +0300

    Update hwa_extension.js

--- hwa_extension.js
+++ hwa_extension.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Подземелье титанов
 // @namespace    http://tampermonkey.net/
-// @version      2026-09-13_v.1.0
+// @version      2026-09-13_v.1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.hero-wars-alliance.com/*
